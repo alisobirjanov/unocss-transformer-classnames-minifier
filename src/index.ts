b0e96fa2eb9ffcba5b17290f279c32d15d6f3454
Alimardon <alisobirzanov29@gmail.com>
2024-01-19 23:56:58 +0500

    Init pkg  0.0.1

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export const defaultChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export function variationsChars(chars: string = defaultChars) {
+  const variation = [-1]
+  const charsLastIdx = chars.length - 1
+
+  const changeArrayElemsFromIdxToEnd = (idx: number) => {
+    for (let i = idx; i < variation.length; i++)
+      variation[i] = 0
+  }
+
+  const getVariation = () => {
+    let idx = 0
+    for (let i = variation.length - 1; i >= 0; i--) {
+      if (variation[i] !== charsLastIdx) {
+        variation[i] += 1
+        idx = i + 1
+        break
+      }
+      if (i === 0) {
+        variation.push(0)
+        break
+      }
+    }
+    changeArrayElemsFromIdxToEnd(idx)
+    return variation
+  }
+
+  return getVariation
+}
+
+const getVar = variationsChars()
+
+for (let i = 0; i < 100; i++)
+  getVar()
