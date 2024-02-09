225b2b43b050e180bef5dcc6c134fd5fe11af045
Alimardon <alisobirzanov29@gmail.com>
2024-02-09 23:05:42 +0500

    feat: playground

---
+++ playground/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import './style.css'
+
+const classes = ['bg-red border-blue hover:border-red']
+</script>
+
+<template>
+  <div>
+    <button class="bg-red border-blue myclassName hover:(bg-gray-400 font-medium) hover:border-red">
+      Click
+    </button>
+    <button :class="classes">
+      Click
+    </button>
+    <button class="bg-blue">
+      Click
+    </button>
+  </div>
+</template>
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,24 +4,16 @@ import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 
-import { transformerCompileClass } from '../src/index'
+import transformerClassnamesMinifier from 'unocss-transformer-classnames-minifier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Unocss({
     transformers: [
-      transformerCompileClass(),
-      transformerVariantGroup()
-    ]
-  }),
-  {
-    name: 'asd',
-    enforce: 'pre',
-    renderDynamicImport(...args) {
-      console.log(args)
-    }
-  }
-],
+      transformerClassnamesMinifier(),
+      transformerVariantGroup(),
+    ],
+  })],
 
 })
 
