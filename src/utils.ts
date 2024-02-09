989e952d021246b906474b18a831609db6d446d6
Alimardon <alisobirzanov29@gmail.com>
2024-02-09 22:54:13 +0500

    feat: added utils function

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ export function variationsChars(chars: string = defaultChars) {
       variation[i] = 0
   }
 
-  const getVariation = () => {
+  return () => {
     let idx = 0
     for (let i = variation.length - 1; i >= 0; i--) {
       if (variation[i] !== charsLastIdx) {
@@ -25,48 +25,12 @@ export function variationsChars(chars: string = defaultChars) {
     changeArrayElemsFromIdxToEnd(idx)
     return variation.map(i => chars[i]).join('')
   }
-
-  return getVariation
 }
 
-// const getVar = variationsChars()
-
-// for (let i = 0; i < 100; i++)
-//   getVar()
-
-
-// function variationsChars() {
-//   const variation = [0]
-//   let currentIdx = 0
-//   const lastIdx = defaultChars.length - 1
-
-//   const changeArrayElemsFromIdxToEnd = (idx: number) => {
-//     for (let i = idx; i < variation.length; i++)
-//       variation[i] = 0
-//   }
-
-//   const getVariation = () => {
-//     if (currentIdx === 0 && variation[0] === lastIdx) {
-//       changeArrayElemsFromIdxToEnd(0)
-//       variation.push(0)
-//       currentIdx = variation.length - 1
-//       return variation
-//     }
-
-//     if (variation[currentIdx] === lastIdx) {
-//       currentIdx -= 1
-//     }
-
-//     variation[currentIdx] += 1
-
-//     return variation
-//   }
-// }
-
-
 // /\$t\(\s*(["'\`])(.*?)\1|\)/gm
-
 
 // /\$t\(\s*(["'\`])(.*?)\1(\,?.*?)\)/gm
 
 // /\$t\(\s*(["'\`])(.*?)\1\s*(\,.*?)?\)/gm
+
+// /(["'`])(.*?)\1/g
