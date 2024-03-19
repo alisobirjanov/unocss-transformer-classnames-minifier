cd00177b087284da8b9e8936be73b9dc6d262b93
Alimardon <alisobirzanov29@gmail.com>
2024-03-20 00:05:53 +0500

    chore: update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 // import './style.css'
 
-const classes = ['bg-red border-blue hover:border-red']
+const classes = [':uno: bg-red border-blue hover:border-red']
 </script>
 
 <template>
   <div>
-    <button class="bg-red border-blue myclassName hover:(bg-gray-400 font-medium) hover:border-red">
+    <button class=":uno: bg-red border-blue myclassName hover:(bg-gray-400 font-medium) hover:border-red">
       Click
     </button>
     <button :class="classes">
       Click
     </button>
-    <button class="bg-blue">
+    <button class=":uno: bg-blue">
       Click
     </button>
   </div>
